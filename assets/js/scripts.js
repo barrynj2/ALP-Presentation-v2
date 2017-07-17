960cc8ea943617001474de8893426870fc2ad787
barrynj2 <barrynj2@miamioh.edu>
2017-07-16 23:01:45 -0400

    Add files via upload

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,4 @@
-/*
+$/*
 Theme Name: IAMX
 Author: Trendy Theme
 Author URL: trendytheme.net
@@ -222,34 +222,6 @@ jQuery(function ($) {
 
 
 
-    // -------------------------------------------------------------
-    // Fit Vids
-    // -------------------------------------------------------------
-    (function () {
-        $(".video-container").fitVids();
-    }());
-
-
-
-    // -------------------------------------------------------------
-    // Vidio auto play
-    // -------------------------------------------------------------
-    (function () {
-    
-    /* Vimeo API: http://developer.vimeo.com/player/js-api */
-    
-        var iframe = document.getElementById('nofocusvideo');
-        // $f == Froogaloop
-        var player = $f(iframe);
-
-        $('.modal').on('hidden.bs.modal', function () {
-        player.api('pause');
-        })
-
-        $('.modal').on('shown.bs.modal', function () {
-        player.api('play');
-        })
-    }());
 
 
 
